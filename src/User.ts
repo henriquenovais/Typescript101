b4ead4a53439a8f24582d17dca59b6a63a831f92
Henrique Oliveira <henriquennoliveira@gmail.com>
2023-12-30 14:40:53 -0300

    feat: adjust typing

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,16 @@
-class User {}
+import { faker } from "@faker-js/faker";
+
+class User {
+  name: string;
+
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.person.firstName();
+  }
+}
+
+export default User;
