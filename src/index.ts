c5d984305ff87aced9f5f122f7eb9dcd5d302bb4
Henrique Oliveira <henriquennoliveira@gmail.com>
2024-01-03 21:03:01 -0300

    feat: adjust code

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,19 @@ import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 
-let map: google.maps.Map;
-async function initMap(): Promise<void> {
-  const { Map } = (await google.maps.importLibrary(
-    "maps"
-  )) as google.maps.MapsLibrary;
-  map = new Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+const importMapsLib = async (): Promise<void> => {
+  (await google.maps.importLibrary("maps")) as google.maps.MapsLibrary;
+};
 
-initMap();
+const main = async (): Promise<void> => {
+  await importMapsLib();
 
-// const user = new User();
-// const company = new Company();
+  const user = new User();
+  const company = new Company();
 
-// const cool = new CustomMap();
-// cool.addMarker(user);
-// cool.addMarker(company);
+  const cool = new CustomMap();
+  cool.addMarker(user);
+  cool.addMarker(company);
+};
+
+main();
